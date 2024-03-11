19395ab959cf3a4b75d5c0f1b334d19861ef50ef
chabberton <chabberton@SJCLT002>
2024-03-12 09:28:12 +1300

    Add project files.

---
+++ WagesApp_L1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WagesApp_L1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
